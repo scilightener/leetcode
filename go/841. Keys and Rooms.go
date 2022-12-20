0460140c7ba308d2a081a7f2fe6f34da6184c4e5
scilightener <nikita.l.boss@gmail.com>
2022-12-20 11:18:23 +0300

    841. Keys and Rooms

---
+++ go/841. Keys and Rooms.go
@@ -0,0 +1,36 @@
+package _go
+
+func canVisitAllRooms(rooms [][]int) bool {
+	graph := map[int][]int{}
+	for room, keys := range rooms {
+		graph[room] = keys
+	}
+	return dfs(graph, 0, len(rooms))
+}
+
+func dfs(graph map[int][]int, start, n int) bool {
+	visited := map[int]struct{}{}
+	cur := []int{start}
+	for len(cur) > 0 {
+		node := cur[len(cur)-1]
+		cur = cur[:len(cur)-1]
+		if _, ok := visited[node]; ok {
+			continue
+		}
+		visited[node] = struct{}{}
+		cur = append(cur, graph[node]...)
+		if len(visited) == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+/*
+complexity:
+	time: O(N) simple dfs
+	space: O(N**2) for graph map (if graph is complete)
+
+the idea is still just dfs. could be bfs as well
+*/
