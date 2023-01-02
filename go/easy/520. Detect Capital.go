c73ba3811d339157625f715ae7db87a5c245e381
scilightener <nikita.l.boss@gmail.com>
2023-01-02 12:29:25 +0300

    520. Detect Capital

---
+++ go/easy/520. Detect Capital.go
@@ -0,0 +1,32 @@
+package _go
+
+func detectCapitalUse(word string) bool {
+	if word[0] < 97 {
+		return allLettersSameCase(word[1:])
+	}
+	return allLettersSameCase(word)
+}
+
+func allLettersSameCase(word string) bool {
+	if len(word) == 0 {
+		return true
+	}
+	isCapital := word[0] < 97
+	for _, l := range word {
+		if isCapital != (l < 97) {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+complexity:
+	time: O(N)
+	space: O(1)
+
+the idea is pretty straight forward
+first, check the first letter:
+	if it is capital, we ensure that the left part of the word is either full lowercase or uppercase
+	if not, check that the whole word is lowercase
+*/
