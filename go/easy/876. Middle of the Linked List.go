811800c48e28601c3d5cd2cfaf0e57e4d1ce41e8
scilightener <nikita.l.boss@gmail.com>
2023-01-05 22:33:28 +0300

    876. Middle of the Linked List

---
+++ go/easy/876. Middle of the Linked List.go
@@ -0,0 +1,16 @@
+package easy
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
