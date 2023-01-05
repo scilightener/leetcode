15c2ce8e279cf2fab2913c34c33000f94e743a13
scilightener <nikita.l.boss@gmail.com>
2023-01-05 22:33:39 +0300

    907. Sum of Subarray Minimums

---
+++ go/medium/907. Sum of Subarray Minimums.go
@@ -0,0 +1,15 @@
+package medium
+
+func sumSubarrayMins(arr []int) int {
+	arr = append(arr, 0)
+	stack, res, j := []int{-1}, 0, 0
+	for i, n := range arr {
+		for len(stack) > 1 && arr[stack[len(stack)-1]] > n {
+			j, stack = stack[len(stack)-1], stack[:len(stack)-1]
+			res += (j - stack[len(stack)-1]) * (i - j) * arr[j]
+			res %= 1000000007
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
