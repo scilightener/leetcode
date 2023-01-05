5543ca0f4246b2cf3b633d85f2e612984e0c75d6
scilightener <nikita.l.boss@gmail.com>
2023-01-05 22:33:28 +0300

    1235. Maximum Profit in Job Scheduling

---
+++ go/hard/1235. Maximum Profit in Job Scheduling.go
@@ -0,0 +1,59 @@
+package hard
+
+import "sort"
+
+type job struct {
+	start, end int
+	profit     int
+}
+
+func jobScheduling(startTime []int, endTime []int, profit []int) int {
+	n := len(startTime)
+
+	jobs := make([]*job, 0, n)
+	for i := range startTime {
+		jobs = append(jobs, &job{
+			start:  startTime[i],
+			end:    endTime[i],
+			profit: profit[i],
+		})
+	}
+	sort.Slice(jobs, func(i, j int) bool {
+		return jobs[i].end < jobs[j].end
+	})
+	dp := make([]int, n)
+	dp[0] = jobs[0].profit
+	for i := 1; i < n; i++ {
+		prof := jobs[i].profit
+		l := binarySearchJobs(jobs, i)
+		if l != -1 {
+			prof += dp[l]
+		}
+		dp[i] = max(prof, dp[i-1])
+	}
+	return dp[n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func binarySearchJobs(jobs []*job, index int) int {
+	l, r := 0, index-1
+	for l <= r {
+		mid := (l + r) / 2
+		if jobs[mid].end <= jobs[index].start {
+			if jobs[mid+1].end <= jobs[index].start {
+				l = mid + 1
+			} else {
+				return mid
+			}
+		} else {
+			r = mid - 1
+		}
+	}
+	return -1
+}
